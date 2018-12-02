25ee8e4d6d6fab55a2ab7d7e5c854dab779876a4
Alexa2411 <sania2411@ukr.net>
2018-12-02 16:38:03 +0200

    fix tests

--- src/test/java/com/trial/calendar/service/CalendarEntryServiceIntTest.java
+++ src/test/java/com/trial/calendar/service/CalendarEntryServiceIntTest.java
@@ -2,17 +2,15 @@ package com.trial.calendar.service;
 
 import com.trial.calendar.CalendarApplication;
 import com.trial.calendar.controller.CalendarEntryController;
-import com.trial.calendar.dao.CalendarEntryDaoImpl;
 import com.trial.calendar.domain.CalendarEntry;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,24 +18,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.sql.DataSource;
-import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CalendarApplication.class)
 public class CalendarEntryServiceIntTest {
 
-    
-    private MockMvc mockMvc;
-    public DataSource dataSource;
-    public JdbcTemplate jdbcTemplate;
 
-    @Mock
-    private CalendarEntryDaoImpl calendarEntryDaoImpl;
-    @Mock
+    private MockMvc mockMvc;
+    @Autowired
+    public DataSource dataSource;
+
+    @Autowired
     private CalendarEntryService calendarEntryServiceImpl;
 
     @InjectMocks
@@ -46,30 +42,65 @@ public class CalendarEntryServiceIntTest {
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(calendarEntryController).build();
+       // this.mockMvc = MockMvcBuilders.standaloneSetup(calendarEntryController).build();
         dataSource = new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
-                .addScripts("/schema.sql","/data.sql")
-
+                .addScripts("/schema.sql", "/data.sql")
                 .build();
-        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Test
-    public void checkFindCalendarEntryById() throws Exception { //todo rename
-        CalendarEntry ce = new CalendarEntry();
-        ce.setId(1L);
-        ce.setDescription("test");
-        ce.setDate(new Date(System.currentTimeMillis()));
-        calendarEntryServiceImpl.addCalendarService(ce) ;       
-        assertThat(calendarEntryServiceImpl.findCalendarEntryById(1L).isPresent()).isTrue();
-       mockMvc.perform(get("/calendar-entries/{id}", 1L)).andExpect(status().isOk());
-
+    public void checkFindCalendarEntryByExistingId() throws Exception {
+        Optional<CalendarEntry> calendarEntryById = calendarEntryServiceImpl.findCalendarEntryById(1L);
+        assertThat(calendarEntryById.isPresent()).isTrue();
+        //Check that description = test BD party (Alice)
+        assertThat(calendarEntryById.get().getDescription()).isEqualTo("test BD party (Alice)");
+        //check that dat = 2012-09-17
+        System.out.println(calendarEntryById.get());
+        assertThat(calendarEntryById.get().getDate().toString()).isEqualTo("2012-09-17");
     }
 
-    @After
-    public void destroy(){
-        jdbcTemplate.execute("drop table calendar_entry");
+    @Test
+    public void checkFindCalendarEntryByNotExistingId() throws Exception {
+        Optional<CalendarEntry> calendarEntryById = calendarEntryServiceImpl.findCalendarEntryById(111L);
+        assertThat(calendarEntryById.isPresent()).isFalse();
     }
+
+    @Test
+    public void checkFindCalendarEntryByExistingDate() throws Exception {
+        java.util.Date date = new SimpleDateFormat("yyyy-mm-dd").parse("2012-01-12");
+        List<CalendarEntry> calendarEntriesByDate = calendarEntryServiceImpl.findCalendarEntriesByDate(new java.sql.Date(date.getTime()));
+        assertThat(calendarEntriesByDate.isEmpty()).isFalse();
+    }
+
+    @Test
+    public void checkFindCalendarEntryByExistingDates() throws Exception {
+        java.util.Date startDate = new SimpleDateFormat("yyyy-mm-dd").parse("2012-01-12");
+        java.util.Date endDate = new SimpleDateFormat("yyyy-mm-dd").parse("2018-01-12");
+        List<CalendarEntry> calendarEntriesByDates = calendarEntryServiceImpl.findCalendarEntriesBetweenDates(new java.sql.Date(startDate.getTime()),
+                new java.sql.Date(endDate.getTime()));
+        System.out.println(calendarEntriesByDates);
+        assertThat(calendarEntriesByDates.isEmpty()).isFalse();
+        assertThat(calendarEntriesByDates.size()==3).isTrue();
+    }
+
+    @Test
+    public void checkFindCalendarEntryByNotExistingDates() throws Exception {
+        java.util.Date startDate = new SimpleDateFormat("yyyy-mm-dd").parse("2006-01-12");
+        java.util.Date endDate = new SimpleDateFormat("yyyy-mm-dd").parse("2008-01-12");
+        List<CalendarEntry> calendarEntriesByDates = calendarEntryServiceImpl.findCalendarEntriesBetweenDates(new java.sql.Date(startDate.getTime()),
+                new java.sql.Date(endDate.getTime()));
+        System.out.println(calendarEntriesByDates);
+        assertThat(calendarEntriesByDates.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void checkFindCalendarEntryByNotExistingDate() throws Exception {
+        java.util.Date date = new SimpleDateFormat("yyyy-mm-dd").parse("2013-07-12");
+        List<CalendarEntry> calendarEntriesByDate = calendarEntryServiceImpl.findCalendarEntriesByDate(new java.sql.Date(date.getTime()));
+        System.out.println(calendarEntriesByDate);
+        assertThat(calendarEntriesByDate.isEmpty()).isTrue();
+    }
+
 
 }
