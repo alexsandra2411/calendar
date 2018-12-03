5ecc5c0218e1433985cf53f4fe62d612c2342d27
Alexa2411 <sania2411@ukr.net>
2018-12-03 12:10:03 +0200

    add mock tests

---
+++ src/test/java/com/trial/calendar/controller/CalendarEntryControllerTest.java
@@ -0,0 +1,87 @@
+package com.trial.calendar.controller;
+
+import com.trial.calendar.CalendarApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.sql.DataSource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CalendarApplication.class)
+public class CalendarEntryControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    public DataSource dataSource;
+
+    @Autowired
+    private CalendarEntryController calendarEntryController;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(calendarEntryController).build();
+        dataSource = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScripts("/schema.sql", "/data.sql")
+                .build();
+    }
+
+    @Test
+    public void checkGetMethodOfCalendarControllerByExistingId() throws Exception {
+        mockMvc.perform(get("/calendar-entries/" + 1L)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void checkGetMethodOfCalendarControllerByNotExistingId() throws Exception {
+        mockMvc.perform(get("/calendar-entries/" + 221L)).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void checkFindCalendarEntryByExistingDate() throws Exception {
+        Date date = getDateFromString("2012-01-12");
+        mockMvc.perform(get("/calendar-entries/by-date/" + date)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void checkFindCalendarEntryByExistingDates() throws Exception {
+        Date startDate = getDateFromString("2012-01-12");
+        Date endDate = getDateFromString("2018-01-12");
+        mockMvc.perform(get("/calendar-entries/by-dates/" + startDate + "/" + endDate)).andExpect(status().isOk());
+    }
+
+    private Date getDateFromString(String dateValue) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-mm-dd").parse(dateValue);
+        return new java.sql.Date(date.getTime());
+    }
+
+    @Test
+    public void checkFindCalendarEntryByNotExistingDates() throws Exception {
+        Date startDate = getDateFromString("2006-01-12");
+        Date endDate = getDateFromString("2008-01-12");
+        mockMvc.perform(get("/calendar-entries/by-dates/" + startDate + "/" + endDate)).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void checkGetCalendarEntryByNotExistingDate() throws Exception {
+        Date date = getDateFromString("2001-01-12");
+        mockMvc.perform(get("/calendar-entries/by-date/" + date)).andExpect(status().isNotFound());
+    }
+}
